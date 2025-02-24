e8f0da5d39df102756e7a64a2b5f807ed48cdf84
Able047 <077bme003.able@pcampus.edu.np>
2025-02-24 23:18:29 +0545

    Fixed mobile responsiveness issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Heading, Text, Button } from "@chakra-ui/react";
+import { Box, Image, Heading, Text, Button, Container } from "@chakra-ui/react";
 import coverPhoto from "../assets/cover.jpg";
 import { keyframes } from "@emotion/react";
 
@@ -22,7 +22,7 @@ const Hero = ({ onExploreClick }) => {
         alt="ANNFSU"
         objectFit="cover"
         w="100%"
-        h={{ base: "300px", md: "400px", lg: "600px" }}
+        h={{ base: "400px", md: "450px", lg: "600px" }}
       />
       <Box
         position="absolute"
@@ -33,31 +33,57 @@ const Hero = ({ onExploreClick }) => {
         bg="black"
         opacity="0.5"
       />
+      <Container 
+        maxW="container.lg" 
+        position="absolute" 
+        top="0" 
+        left="0" 
+        right="0" 
+        height="100%"
+        centerContent
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
         <Box
-          position="absolute"
-          top="50%"
-          left="50%"
-          transform="translate(-50%, -50%)"
           textAlign="center"
           color="white"
+          width="100%"
+          px={{ base: 4, md: 8 }}
         >
           <Heading
             as="h1"
-            size={{ base: "l", md: "2xl" }}
-            mb="4"
+            fontSize={{ base: "xl", sm: "2xl", md: "3xl", lg: "4xl" }}
+            mb={{ base: 2, md: 4 }}
             animation={`${fadeIn} 2s ease-in-out`}
+            lineHeight="1.3"
           >
             अखिल नेपाल राष्ट्रिय स्वतन्त्र विद्यार्थी युनियन
           </Heading>
-          <Text fontSize="lg" mb="6" animation={`${fadeIn} 2s ease-in-out`}>
+          <Text 
+            fontSize={{ base: "sm", sm: "md", md: "lg" }} 
+            mb={{ base: 4, md: 6 }} 
+            animation={`${fadeIn} 2s ease-in-out`}
+            maxW="800px"
+            mx="auto"
+            lineHeight="1.6"
+          >
             अनेरास्ववियु : स्थापनाकाल देखिनै विद्यार्थी हकहितको लागि समर्पित
             संगठन हो। देशको हरेक परिवर्तनमा विद्यार्थीको योगदानलाई संस्थागत
             गर्दै राष्ट्र र राष्ट्रियताको निम्ति सदैव सतीसालझैँ निर्भिक छ।
           </Text>
-          <Button colorScheme="red" size="lg" onClick={onExploreClick}>
+          <Button 
+            colorScheme="red" 
+            size={{ base: "md", md: "lg" }} 
+            onClick={onExploreClick}
+            animation={`${fadeIn} 2s ease-in-out 0.5s`}
+            opacity="0"
+            sx={{ animationFillMode: "forwards" }}
+          >
             हाम्रो इतिहास
           </Button>
         </Box>
+      </Container>
     </Box>
   );
 };
